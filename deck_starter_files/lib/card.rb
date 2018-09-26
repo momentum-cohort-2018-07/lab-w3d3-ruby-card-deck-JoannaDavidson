3b475929c6638e20083db19d26107bd739cca613
Joanna Davidson <joanna@shrikesong.com>
2018-09-26 16:56:44 -0400

    Created card and deck

--- deck_starter_files/lib/card.rb
+++ deck_starter_files/lib/card.rb
@@ -1,2 +1,24 @@
 class Card
+    attr_accessor :rank, :suit, :other_card
+
+    def initialize (rank, suit)
+        @card = []
+        @rank = rank
+        @suit = suit
+        @face_value = [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
+    end
+
+    def value
+        @face_value.index(rank)
+    end
+
+    def == (other_card)
+        self.rank == other_card.rank &&
+        self.suit == other_card.suit
+      end
+
+    def greater_than? (other_card)
+        self.value > other_card.value
+    end
+
 end
